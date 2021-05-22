7af5d879db073c69801b194bbe13c74c6a7881f4
suelitonoliveira <suelitondeoliveira@gmail.com>
2021-05-22 09:01:17 -0300

    Ajuste

--- src/capitulo3/Dialog1.java
+++ src/capitulo3/Dialog1.java
@@ -1,12 +1,18 @@
 package capitulo3;
 
+import java.util.Scanner;
+
 import javax.swing.JOptionPane;
 
 public class Dialog1 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
 		JOptionPane.showMessageDialog(null, "Welcome to java");
+		
+		
+		sc.close();
 
 	}
 
