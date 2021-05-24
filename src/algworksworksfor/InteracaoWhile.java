a77ee17dfec6b258570105fe3f0c626cd3b7488d
suelitonoliveira <suelitondeoliveira@gmail.com>
2021-05-24 20:38:18 -0300

    Estudando while

---
+++ src/algworksworksfor/InteracaoWhile.java
@@ -0,0 +1,16 @@
+package algworksworksfor;
+
+public class InteracaoWhile {
+
+	public static void main(String[] args) {
+		/*
+		 * for (int i = 0; i < 10; i++) { System.out.println(i); }
+		 */
+		int i = 0;
+		while (i < 10) {
+			System.out.println("Um texto qualquer.");
+			i++;
+		}
+	}
+
+}
